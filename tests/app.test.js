7e47266bdd2c299a7ecfff7abea12af087a14745
Mohit Kumar Bhagi <mohitbhagi248@gmail.com>
2024-09-01 11:24:16 -0400

    Unit-Test

--- tests/app.test.js
+++ tests/app.test.js
@@ -2,15 +2,19 @@ const request = require('supertest')
 const app = require("../app")
 
 describe('API Endpoints', ()=>{
+    // Check status with GET
     it('It should return 200', async()=>{
         const res=await request(app).get('/api');
         expect(res.statusCode).toEqual(200);
     });
 
+    // Checking the property message exist using message
     it('It should return valid message', async()=>{
         const res=await request(app).get('/api');
         expect(res.body).toHaveProperty('message');
     });
+
+    // Post data using POST
     it('It should post valid data', async () => {
         const data = { data: "Hello" };
         const res = await request(app)
